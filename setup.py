91d1fc5d90305f5235d7523f6b38c7f220ffabcc
Wanderson Gonçalves Nunes <wanderson@sentinella.com.br>
2019-12-06 15:56:53 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(name = "nscan",
       author = "Sentinella Enterprise",
       author_email = "suporte@sentinella.com.br",
       url = "https://github.com/sentinella-enterprise/nscan",
-      entry_points = {"console_scripts": ["nscan=nscan.__main__:main"]},
+      entry_points = {"console_scripts": ["nscan=scan.__main__:main"]},
       install_requires = DEPENDENCIES,
       keywords = ["security", "network", "scanner", "nmap"])
