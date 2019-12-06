f653e82824ab5b882227eddb4e0ed33175963449
Wanderson Gonçalves Nunes <wanderson@sentinella.com.br>
2019-12-06 15:54:39 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+DEPENDENCIES = open("requirements.txt", "r").read().split("\n")
+README = open("README.md", "r").read()
+
+setup(name = "nscan",
+      version = "0.0.1",
+      description = "",
+      long_description = README,
+      long_description_content_type = "text/x-md",
+      author = "Sentinella Enterprise",
+      author_email = "suporte@sentinella.com.br",
+      url = "https://github.com/sentinella-enterprise/nscan",
+      entry_points = {"console_scripts": ["nscan=nscan.__main__:main"]},
+      install_requires = DEPENDENCIES,
+      keywords = ["security", "network", "scanner", "nmap"])
